328551412b676a93b1f094129bdbbfb1bb449925
YCHuang <a9981321@gmail.com>
2025-07-14 22:02:25 +0800

    hello

--- BasePawn.h
+++ BasePawn.h
@@ -14,6 +14,9 @@ class TOONTANKS_API ABasePawn : public APawn
 public:
 	// Sets default values for this pawn's properties
 	ABasePawn();
+
+	//Hello github
+	FString github = "Hello GitHub";
 	
 	UPROPERTY(VisibleAnywhere)
 	int32 VisibleAnywhereInt = 12;
